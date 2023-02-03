6f8c26796fb27f55dc24e21a68aa5e7100a6235a
suraj292 <surajkumarsharma123@gmail.com>
2023-02-03 19:57:59 +0530

    cart, review, slug migration

---
+++ database/migrations/2023_02_03_141205_create_slug_urls_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('slug_urls', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->nullable()->constrained('products')->onDelete('cascade');
+            //$table->string('collection_id')->nullable();
+            //$table->string('blog_id')->nullable();
+            $table->string('slug_title');
+            $table->longText('header')->nullable();
+            $table->longText('footer')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('slug_urls');
+    }
+};
---
+++ database/migrations/2023_02_03_142053_create_carts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->foreignId('product_color_id')->constrained('product_color_images')->onDelete('cascade');
+            $table->integer('quantity')->default(1);
+            $table->string('image');
+            $table->decimal('price')->nullable();
+            $table->decimal('offer_price')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+};
