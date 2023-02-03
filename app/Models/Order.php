84daa7896cff4d8d4615961d43199b3babb13e13
suraj292 <surajkumarsharma123@gmail.com>
2023-02-03 23:43:33 +0530

    blog migration left only

---
+++ app/Models/Order.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+      'payment_id',
+      'user_id',
+      'user_addresses_id',
+      'logistic_id',
+      'user_cart_ids',
+      'coupon_code',
+      'coupon_discount',
+      'invoice_number',
+      'order_number',
+      'cod_charge',
+      'total_payment',
+      'gst_charge',
+      'dispatch',
+      'delivered',
+    ];
+}
---
+++ database/migrations/2023_02_03_180110_create_orders_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('payment_id')->nullable();
+            $table->foreignId('user_id')->nullable()->constrained('users');
+            $table->foreignId('user_addresses_id')->constrained('user_addresses');
+            $table->string('logistic_id')->nullable();
+            $table->string('user_cart_ids');
+            $table->string('coupon_code')->nullable();
+            $table->string('coupon_discount')->nullable();
+            $table->string('invoice_number')->nullable();
+            $table->string('order_number')->nullable();
+            $table->string('cod_charge')->nullable();
+            $table->string('total_payment');
+            $table->string('gst_charge');
+            $table->boolean('dispatch')->default(0);
+            $table->boolean('delivered')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
